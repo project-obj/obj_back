d6845fc48147ff631096960c0be25511bacfef29
tjrgh <tjrgh88@gmail.com>
2023-06-13 11:02:04 +0900

    express

---
+++ obj_be/app.js
@@ -0,0 +1,43 @@
+const createError = require('http-errors');
+const express = require('express');
+const path = require('path');
+const cookieParser = require('cookie-parser');
+const logger = require('./lib/logger');
+const bodyParser = require('body-parser');
+const cors = require('cors');
+const corsConfig = require('./config/corsConfig.json');
+const indexRouter = require('./routes/index');
+
+app.use(cors(corsConfig));
+const app = express();
+logger.info('app start');
+// view engine setup
+app.set('views', path.join(__dirname, 'views'));
+app.set('view engine', 'ejs');
+
+app.use(express.json());
+app.use(express.urlencoded({ extended: false }));
+app.use(cookieParser());
+app.use(express.static(path.join(__dirname, 'public')));
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.use('/', indexRouter);
+
+// catch 404 and forward to error handler
+app.use(function (req, res, next) {
+  next(createError(404));
+});
+
+// error handler
+app.use(function (err, req, res, next) {
+  // set locals, only providing error in development
+  res.locals.message = err.message;
+  res.locals.error = req.app.get('env') === 'development' ? err : {};
+
+  // render the error page
+  res.status(err.status || 500);
+  res.render('error');
+});
+
+module.exports = app;
